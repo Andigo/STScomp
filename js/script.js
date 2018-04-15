8d8d15d0933a452423cebf3d232a52db04076d6a
Valentin <dreamofandroid@gmail.com>
2018-04-15 22:17:01 +0600

    add catalogue

--- js/script.js
+++ js/script.js
@@ -128,3 +128,38 @@ $(document).on("scroll", function(){
 	})	
 }
 
+$(document).ready(function(){
+  $('.catalogue-part-line').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 960,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 780,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 560,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
+});
